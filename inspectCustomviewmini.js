b4cfb7c33e28f2171f549fa33f0aa0782fe3bd19
theoraclenyt <ISHESENSEI@GMAIL.COM>
2026-06-30 19:39:30 +0530

    name and label of custom view

--- inspectCustomviewmini.js
+++ inspectCustomviewmini.js
@@ -1,22 +1,19 @@
-bindClass("android.widget.FrameLayout");bindClass("android.widget.LinearLayout");bindClass("android.widget.TextView");bindClass("android.widget.ScrollView");bindClass("android.graphics.Color");bindClass("android.view.Gravity");bindClass("android.view.View");
+bindClass("android.widget.LinearLayout");
+bindClass("android.widget.TextView");
+bindClass("android.graphics.Color");
 
-var ctx=LL.getContext();
-var root=new LinearLayout(ctx);root.setOrientation(1);root.setPadding(10,10,10,10);
+var ctx = LL.getContext();
+var root = new LinearLayout(ctx);
+root.setOrientation(1);
 
-function add(label,val){
-    var r=new LinearLayout(ctx);r.setPadding(0,1,0,1);
-    var l=new TextView(ctx);l.setText(label+": ");l.setTextColor(Color.WHITE);l.setTextSize(9);
-    var v=new TextView(ctx);v.setText(val===null?"null":val===undefined?"undefined":String(val));v.setTextColor(Color.parseColor("#FF00FF00"));v.setTextSize(9);
-    r.addView(l);r.addView(v);root.addView(r);
-}
+var t1 = new TextView(ctx);
+t1.setText("Name: " + item.getName());
+t1.setTextColor(Color.WHITE);
+root.addView(t1);
 
-var d=item;
-
-add("toString",""+d);
-add("getLabel",d.getLabel());
-add("getName",d.getName());
-add("getTag()",d.getTag());
-add("getTag(label)",d.getTag("label"));
-add("getTag(name)",d.getTag("name"));
+var t2 = new TextView(ctx);
+t2.setText("Label: " + item.getLabel());
+t2.setTextColor(Color.WHITE);
+root.addView(t2);
 
 return root;
